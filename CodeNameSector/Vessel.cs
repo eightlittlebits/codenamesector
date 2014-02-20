5915c26b16032ef3d31469929690c9c7044b4ed3
David Parrott <david.parrott@gmail.com>
2014-02-20 11:44:34 +0000

    Add Vessel class and tests.

---
+++ CodeNameSector/Vessel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeNameSector
+{
+    public class Vessel
+    {
+        public Vector2 Position { get; private set; }
+        public Vector2 Bearing { get; protected set; }
+        public int Speed { get; protected set; }
+
+        public Vessel(Vector2 position, Vector2 bearing, int speed)
+        {
+            Position = position;
+            Bearing = bearing;
+            Speed = speed;
+        }
+
+        public void Move()
+        {
+            Position = Position + Bearing * Speed;
+        }
+    }
+}
---
+++ CodeNameSectorTests/VesselTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CodeNameSector;
+using NUnit.Framework;
+
+namespace CodeNameSectorTests
+{
+    [TestFixture]
+    public class VesselTests
+    {
+        [Test]
+        public void Vessel_Move_MovesCorrectDistanceAndDirection()
+        {
+            Vector2 position = new Vector2(35, 25); // 35N, 25E
+            Vector2 bearing = new Vector2(1, 1);    // NE
+            const int speed = 4;
+            
+            Vessel v = new Vessel(position, bearing, speed);
+
+            v.Move();
+
+            Vector2 expectedPosition = new Vector2(39, 29);
+
+            Assert.That(v.Position, Is.EqualTo(expectedPosition));
+        }
+    }
+}
