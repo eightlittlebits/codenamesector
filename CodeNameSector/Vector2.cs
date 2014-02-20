650685345f98994fa2cdd7e43873d0137e8dbdf1
David Parrott <david.parrott@gmail.com>
2014-02-20 09:38:51 +0000

    Added integer Vector2 class.

---
+++ CodeNameSector/Vector2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodeNameSector
+{
+    public struct Vector2
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Vector2(int x, int y)
+            : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector2 operator -(Vector2 v1, Vector2 v2)
+        {
+            return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public static Vector2 operator *(Vector2 v1, int i)
+        {
+            return new Vector2(v1.X * i, v1.Y * i);
+        }
+
+        public static int ChebyshevDistance(Vector2 v1, Vector2 v2)
+        {
+            var distance = v1 - v2;
+
+            return Math.Max(Math.Abs(distance.X), Math.Abs(distance.Y));
+        }
+    }
+}
